80a002b6cc58c61505d416fd5239f17858483bb2
Chase737 <59313163+Chase737@users.noreply.github.com>
2022-01-22 16:05:48 +0800

    Add files via upload

--- reciver.sh
+++ reciver.sh
@@ -1,11 +1,8 @@
 #! /bin/bash
-
-# RX IP Setting
-sudo ifconfig wlan1 192.168.6.6 netmask 255.255.255.0
+sudo ifconfig wlan1 192.168.6.6 netmask 255.255.255.0		# the ip address of hmb pro
 
 ip addr | grep 192.168.6.6
 
-# receiving network packages
 while [ 1 ]
 do	
 	time_stp="sample-"`date "+%Y-%m-%d-%H-%M-%S"`".dat"
